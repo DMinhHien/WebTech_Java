d396b8a3f7a09c7af37042046c69747fc33797b1
MinhHien <150352316+DMinhHien@users.noreply.github.com>
2025-06-10 22:02:48 +0700

    Update FireBaseConfig.ts

--- FrontEnd_Java/E-commerce/src/FireBaseConfig.ts
+++ FrontEnd_Java/E-commerce/src/FireBaseConfig.ts
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEnQxAyFT6vBVYiOrnSxRZhDudHDXeXpU",
-  authDomain: "webbandocongnghe-ad282.firebaseapp.com",
-  projectId: "webbandocongnghe-ad282",
-  storageBucket: "webbandocongnghe-ad282.firebasestorage.app",
-  messagingSenderId: "128230412321",
-  appId: "1:128230412321:web:44d577849ca91986bad360",
-  measurementId: "G-HVR5S3JH05"
+  apiKey: "AIzaSyC2FcKkb6DP8URpNrEGwh-zIEdCROYUJFQ",
+  authDomain: "didong-foodapp-1465e.firebaseapp.com",
+  databaseURL: "https://didong-foodapp-1465e-default-rtdb.firebaseio.com",
+  projectId: "didong-foodapp-1465e",
+  storageBucket: "didong-foodapp-1465e.appspot.com",
+  messagingSenderId: "267636866368",
+  appId: "1:267636866368:web:2b3ff782174d63d6274ea5",
+  measurementId: "G-ZLTY0P0CHX"
 };
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
